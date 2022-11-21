d0fd9036b34b3c9a1e18e2ea209650921a9d469c
Daniel Villarreal <danielvillarreal441@gmail.com>
2022-11-20 23:44:17 -0600

    Fixes

--- src/views/TestingAuth.vue
+++ src/views/TestingAuth.vue
@@ -1,12 +1,5 @@
 <template>
   <div class="testing-auth top-margin">
-    <!-- Check that the SDK client is not currently loading before accessing is methods -->
-    <!-- <div v-if="!$auth.loading"> -->
-    <!-- show login when not authenticated -->
-    <!-- <button v-if="!$auth.isAuthenticated" @click="login">Log in</button> -->
-    <!-- show logout when authenticated -->
-    <!-- <button v-if="$auth.isAuthenticated" @click="logout">Log out</button> -->
-    <!-- </div> -->
     <h1>Testing Client Storage</h1>
     <v-data-table
       :headers="headers"
